7b5ebee3090759988a13c40c73aaf7504c230546
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-03-10 16:22:51 +0900

    add PaymentRepository for accessing payment data

---
+++ src/main/kotlin/com/payments/payment/repository/PaymentRepository.kt
@@ -0,0 +1,9 @@
+package com.payments.payment.repository
+
+import com.payments.payment.domain.Payment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PaymentRepository : JpaRepository<Payment, Long> {
+    fun findByOrderId(orderId: String): Payment?
+    fun findByIdempotencyKey(idempotencyKey: String): Payment?
+}
