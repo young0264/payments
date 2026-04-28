24325ea97965cf9a31f9d55ea44dbdc720a3e912
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-04-29 00:34:17 +0900

    feat: refactor PaymentsApplicationTests to extend AbstractIntegrationTest

--- src/test/kotlin/com/payments/PaymentsApplicationTests.kt
+++ src/test/kotlin/com/payments/PaymentsApplicationTests.kt
@@ -1,10 +1,8 @@
 package com.payments
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class PaymentsApplicationTests {
+class PaymentsApplicationTests : AbstractIntegrationTest() {
 
 	@Test
 	fun contextLoads() {
