3bce05a4db87819ee7a06fcb481cd445a51ac564
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-04-29 17:42:38 +0900

    feat: update merchant table to add unique api_key column

--- src/main/resources/db/migration/V5__create_merchant_table.sql
+++ src/main/resources/db/migration/V5__create_merchant_table.sql
@@ -4,6 +4,3 @@ CREATE TABLE merchant
     name       VARCHAR(100) NOT NULL,
     created_at DATETIME     NOT NULL
 );
-
-INSERT INTO merchant (name, created_at) VALUES ('테스트 가맹점 A', NOW());
-INSERT INTO merchant (name, created_at) VALUES ('테스트 가맹점 B', NOW());
